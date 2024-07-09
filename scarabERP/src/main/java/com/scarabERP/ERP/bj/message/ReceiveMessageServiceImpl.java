2dfd0add4dbe8231acca6634feb621c188cddac1
nltpbj <nltpbj1725@naver.com>
2024-07-09 12:19:59 +0900

    수정

--- scarabERP/src/main/java/com/scarabERP/ERP/bj/message/ReceiveMessageServiceImpl.java
+++ scarabERP/src/main/java/com/scarabERP/ERP/bj/message/ReceiveMessageServiceImpl.java
@@ -2,19 +2,23 @@ package com.scarabERP.ERP.bj.message;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class ReceiveMessageServiceImpl implements ReceiveMessageService{
+	@Autowired
+	ReceiveMessageDAO rdao;
 	
 	@Autowired
-	ReceiveMessageDAO dao;
+	SendMessageDAO sdao;
 	
-	@Transactional
 	@Override
-	public ReceiveMessageVO readReceive(int receive_message_id) {
+	public void insert(MessageVO vo) {
+		rdao.insert(vo);
+		sdao.insert(vo);
 		
-		return null;
-	}	
+	}
+	
+	
+
 	
 }
