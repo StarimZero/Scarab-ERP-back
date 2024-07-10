36dba148ded4b45a79c82385fa35782452871d9a
nltpbj <167416209+nltpbj@users.noreply.github.com>
2024-07-10 15:33:49 +0900

    Bj (#19)
    * message
    
    * 메세지송신
    
    * 수정
    
    * dao service 수정
    
    * title추가
    
    * vo변경

--- scarabERP/src/main/java/com/scarabERP/ERP/bj/message/ReceiveMessageDAO.java
+++ scarabERP/src/main/java/com/scarabERP/ERP/bj/message/ReceiveMessageDAO.java
@@ -1,6 +1,8 @@
 package com.scarabERP.ERP.bj.message;
 
+
 import java.util.List;
+
 
 
 public interface ReceiveMessageDAO {
@@ -8,7 +10,7 @@ public interface ReceiveMessageDAO {
 	public List<MessageVO> list(String message_receiver);
 	public void updateReceiveState(MessageVO vo);
 	public void delete(int message_id);
-	public void updateReadDate(int message_id);
+	public void updateReadDate(MessageVO vo);
 	public MessageVO readReceive(int message_id);
 }
 
