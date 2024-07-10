8e9b8274f8cc0549782a546f986f11587401ab13
coding-faster <berichmore@naver.com>
2024-07-10 18:40:03 +0900

    jay

--- scarabERP/src/test/java/com/scarabERP/ERP/ScarabErpApplicationTests.java
+++ scarabERP/src/test/java/com/scarabERP/ERP/ScarabErpApplicationTests.java
@@ -1,13 +1,21 @@
 package com.scarabERP.ERP;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.scarabERP.ERP.jay.BBS.BBSDAO;
 
 @SpringBootTest
 class ScarabErpApplicationTests {
-
+	@Autowired
+	BBSDAO dao;
+	
 	@Test
 	void contextLoads() {
+		
+		
+		
 	}
 
 }
