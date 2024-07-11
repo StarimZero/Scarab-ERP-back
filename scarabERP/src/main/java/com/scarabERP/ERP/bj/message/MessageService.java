50b228cc977642e459b27ffa2454f9d2739321cc
nltpbj <167416209+nltpbj@users.noreply.github.com>
2024-07-11 18:16:30 +0900

    메신저 (#26)
    * message
    
    * 메세지송신
    
    * 수정
    
    * dao service 수정
    
    * title추가
    
    * vo변경

--- scarabERP/src/main/java/com/scarabERP/ERP/bj/message/MessageService.java
+++ scarabERP/src/main/java/com/scarabERP/ERP/bj/message/MessageService.java
@@ -1,5 +1,10 @@
 package com.scarabERP.ERP.bj.message;
 
+import java.util.HashMap;
+
+import com.scarabERP.ERP.common.QueryVO;
+
 public interface MessageService {
 	public void insert(MessageVO vo);
+	public HashMap<String, Object> deleteList(QueryVO vo, String message_sender, String message_receiver);
 }
--- scarabERP/src/main/java/com/scarabERP/ERP/bj/message/ReceiveMessageController.java
+++ scarabERP/src/main/java/com/scarabERP/ERP/bj/message/ReceiveMessageController.java
@@ -1,5 +1,6 @@
 package com.scarabERP.ERP.bj.message;
 
+import java.util.HashMap;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,9 +9,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-
+import com.scarabERP.ERP.common.QueryVO;
 
 @RestController
 @RequestMapping("/erp/receivemessage")
@@ -32,6 +34,16 @@ public class ReceiveMessageController {
 	public List<MessageVO> list(@PathVariable("message_receiver") String message_receiver) {
 		return dao.list(message_receiver);
 	}
+	
+
+
+	@GetMapping("/deletelist/{message_receiver}")
+	public HashMap<String, Object> deletelist(QueryVO vo, @PathVariable("message_receiver") String message_receiver) {
+		HashMap<String, Object> map = new HashMap<>();
+		map.put("total", dao.total(vo));
+		map.put("deleteList", dao.deleteList(vo, message_receiver));
+		return map;
+	}
     
     
 	
@@ -40,9 +52,9 @@ public class ReceiveMessageController {
 		dao.delete(message_id);
 	}
 	
-	@PostMapping("/update/receive/state")
-	public void updateReceiveState(@RequestBody MessageVO vo) {
-		dao.updateReceiveState(vo);
+	@PostMapping("/update/receive/state/{message_id}")
+	public void updateReceiveState(@PathVariable("message_id") int message_id ) {
+		dao.updateReceiveState(message_id);
 	}
 	
 	@GetMapping("/read/{message_id}")
@@ -55,4 +67,8 @@ public class ReceiveMessageController {
 		dao.updateReadDate(vo);
 	}
 	
+	@PostMapping("/reset/state/{message_id}")
+	public void resetState(@PathVariable("message_id") int message_id) {
+		dao.resetState(message_id);
+	}
 }
