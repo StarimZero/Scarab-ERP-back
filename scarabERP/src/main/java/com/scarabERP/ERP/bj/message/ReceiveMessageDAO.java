32fb8b3bb8795fbee14efcedbc8644d09847fde9
nltpbj <nltpbj1725@naver.com>
2024-07-26 18:06:44 +0900

    주석설명

--- scarabERP/src/main/java/com/scarabERP/ERP/bj/message/ReceiveMessageDAO.java
+++ scarabERP/src/main/java/com/scarabERP/ERP/bj/message/ReceiveMessageDAO.java
@@ -10,17 +10,17 @@ import com.scarabERP.ERP.common.QueryVO;
 
 
 public interface ReceiveMessageDAO {
-	public void insert(MessageVO vo);
-	public List<HashMap<String,Object>> list(QueryVO vo, String message_receiver);
-	public void updateReceiveState(int message_id);
-	public void delete(int message_id);
-	public void updateReadDate(int message_id);
-	public MessageVO readReceive(int message_id);
-	public List<HashMap<String,Object>> deleteList(QueryVO vo, String message_receiver);
-	public int total(String message_receiver);
-	public int dtotal(String message_receiver);
-	public void resetState(int message_id);
-	public List<HashMap<String,Object>> nlist(QueryVO vo, String message_receiver);
-	public int ntotal(String message_receiver);
+	public void insert(MessageVO vo); //메세지 보내기 서비스 사용
+	public List<HashMap<String,Object>> list(QueryVO vo, String message_receiver); //받은메세지 전체리스트
+	public void updateReceiveState(int message_id); // 휴지통으로 이동
+	public void delete(int message_id); // 휴지통에서 삭제
+	public void updateReadDate(int message_id); //읽은 시간 업데이트
+	public MessageVO readReceive(int message_id); // 읽은 시간
+	public List<HashMap<String,Object>> deleteList(QueryVO vo, String message_receiver); //받은메세지 휴지통리스트
+	public int total(String message_receiver);//받은메세지 토탈
+	public int dtotal(String message_receiver);// 받은메세지 휴지통 토탈
+	public void resetState(int message_id);// 휴지통에서 받은메세지로 이동
+	public List<HashMap<String,Object>> nlist(QueryVO vo, String message_receiver);// 안읽은 메세지 리스트
+	public int ntotal(String message_receiver);// 안읽은 메세지 토탈
 }
 
